283ddae09dfc2e282470513ca26552180a01cd2d
Michael Howard <mike.gene.howard@gmail.com>
2022-07-21 07:21:18 -1000

    creates migration file

--- treasure_bay/db/migration.sql
+++ treasure_bay/db/migration.sql
@@ -1,42 +1,40 @@
--- DROP DATABASE IF EXISTS adventure_wise;
+DROP DATABASE IF EXISTS treasure_bay;
 
--- CREATE DATABASE adventure_wise;
--- \c adventure_wise
+CREATE DATABASE treasure_bay;
+\c adventure_wise
 
 DROP TABLE IF EXISTS user CASCADE;
 DROP TABLE IF EXISTS product CASCADE;
 DROP TABLE IF EXISTS message CASCADE;
 
 
--- CREATE TABLE users(
---     user_id SERIAL PRIMARY KEY,
---     first_name VARCHAR(100) NOT NULL,
---     last_name VARCHAR(150) NOT NULL,
---     email VARCHAR(150) NOT NULL,
---     password TEXT NOT NULL
--- );
+CREATE TABLE user(
+    user_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(150) NOT NULL,
+    city TEXT NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR(16) NOT NULL
+);
 
--- CREATE TABLE trip(
---     trip_id SERIAL PRIMARY KEY,
---     trip_name TEXT,
---     user_id INT REFERENCES users(user_id) ON DELETE CASCADE
--- );
+CREATE TABLE product(
+    product_id SERIAL PRIMARY KEY,
+    name VARCHAR(256),
+    price NUMERIC NOT NULL,
+    description TEXT NOT NULL,
+    details TEXT,
+    picture TEXT,
+    user_id INT REFERENCES user(user_id) ON DELETE CASCADE
+);
 
--- CREATE TABLE place(
---     place_id SERIAL PRIMARY KEY,
---     place_name VARCHAR(255),
---     place_address VARCHAR(255),
---     trip_date DATE,
---     trip_time TIMESTAMP,
---     notes TEXT,
---     trip_id INT REFERENCES trip(trip_id) ON DELETE CASCADE
--- );
+CREATE TABLE message(
+    message_id SERIAL PRIMARY KEY,
+    conversation_id INT
+    message text,
+    time TIMESTAMP,
+    sender_id INT REFERENCES user(user_id) ON DELETE CASCADE,
+    receiver_id INT REFERENCES user(user_id) ON DELETE CASCADE
+);
 
--- CREATE TABLE popular_places(
---     id SERIAL PRIMARY KEY,
---     city TEXT,
---     state_country TEXT,
---     picture TEXT
--- );
-
--- \i seed.sql
+\i seed.sql
