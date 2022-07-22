19ac7aa5137f55e14936ae8aaf88757b2f37bd5f
Aaron <aaron@Aarons-Air.lan>
2022-07-22 09:34:54 -0400

    added layout and styling to messaging section

--- treasure_bay/src/components/Messages/Messages.js
+++ treasure_bay/src/components/Messages/Messages.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import MessagingInput from './MessagingInput'
+import './Messages.css'
 
 function Messages() {
+
+  const messageSent = ['hello my name is aaron']
+  const MessagesSent = messageSent.map((message, index) => {
+    return (
+
+      <p key={index} className='from-me'>{message}</p>
+    )
+  })
+
   return (
-    <div className='messages'>Messages</div>
+    <div className='messaging-container-test'>
+      <div className='messages-wrapper'>
+        <div className='message-sent'>{MessagesSent}</div>
+      </div>
+      <div className='typing-container'>
+        <MessagingInput />
+      </div>
+    </div>
   )
 }
 
-export default Messages;
+export default Messages
