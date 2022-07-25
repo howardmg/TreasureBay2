d22ad9ec3c848366e9deb421d6ec0a24faf00758
Yanchao Jin <yanchaojin@YanchaodeiMac.local>
2022-07-25 09:31:45 -0500

    update

--- treasure_bay/server.js
+++ treasure_bay/server.js
@@ -58,7 +58,7 @@ app.post("/createproducts", async (req, res) => {
   const { name, price, description, details, image_url, user_id} = req.body;
   try {
        await pool.connect()
-       const addProduct = await pool.query('INSERT INTO products (name, price, description, details, image_url,user_id) VALUES ($1, $2, $3, $4, $5, $6);',
+       const addProduct = await pool.query('INSERT INTO products (name, price, description, details, image_url,user_id) VALUES ($1, $2, $3, $4, ARRAY[$5], $6);',
        [name,price,description, details, image_url,user_id])
         res.status(200).json(addProduct.rows);
      
--- treasure_bay/src/components/ProductItem/PostItemPage.js
+++ treasure_bay/src/components/ProductItem/PostItemPage.js
@@ -5,11 +5,12 @@ import { useState } from 'react';
 
 function PostItemPage() {
      
-            const [name, setname]=useState('test')
-            const [price, setprice]=useState(100.00)
-            const [details, setdetails]=useState('test')
-            const [description, setdescription]=useState('test')
-            const [image, setimage]=useState('https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg')
+            const [name, setname]=useState('flag')
+            const [price, setprice]=useState(200.00)
+            const [details, setdetails]=useState('american flag')
+            const [description, setdescription]=useState('blue and white')
+            const [image, setimage]=useState(['https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg',
+            'https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg'])
             
                                                                   
 
@@ -89,19 +90,19 @@ function addNewProduct(data){
       <div class="col-75">
          
           
-          {/* <form class="box" method="post" action="" enctype="multipart/form-data"  >
-          {/* <Dropzone/> */}
-          {/* <div class="box__input"  >
+          <form class="box" method="post" action="" enctype="multipart/form-data"  >
+     
+           <div class="box__input"  >
           
                 <input class="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
                 
                 <span class="box__dragndrop"> or drag it here</span>
-                <button class="box__button" type="submit">Upload</button>
+                <button class="box__button" type="submit" onClick={addNewProduct}>Upload</button>
           </div>
           <div class="box__uploading">Uploading…</div>
           <div class="box__success">Done!</div>
           <div class="box__error">Error! <span></span>.</div>
-          </form>   */}
+          </form>  
       </div>
 
       <div class="row">
