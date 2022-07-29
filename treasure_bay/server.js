9987cea9d020e3c86647fd85e447f9c9765cc162
jsmith3763 <jsmith3763@gmail.com>
2022-07-29 16:37:51 -0700

    Added create profile route

--- treasure_bay/server.js
+++ treasure_bay/server.js
@@ -100,13 +100,16 @@ app.get("/messages", async (req, res) => {
 
 //=======================================Profile Routes Start===============================================================================================
 
-app.post(`/createprofile`, upload.single("file"), async (req, res, next) => {
+app.post(`/createprofile`, upload.array("file"), async (req, res, next) => {
   try {
-    const avatar = req.file;
+    const avatar = req.files;
     const result = await uploadFile(avatar)
-    const imageURL = result.Location;
+    console.log(avatar)
+    const imgKey = avatar[0].filename;
+    const imageURL = `https://treasure-bay-images.s3.amazonaws.com/${imgKey}`;
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
+    console.log(req.file)
+    console.log(req.body)
     await db.query(
       `INSERT INTO users (first_name, last_name, city, state, zipcode, email, password, avatar) VALUES ('${req.body.first_name}', '${req.body.last_name}', '${req.body.city}', '${req.body.state}', '${req.body.zipcode}', '${req.body.email}', '${hashedPassword}', '${imageURL}');`
     );
