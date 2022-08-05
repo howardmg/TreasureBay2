a7e7006f7d76b3d204cac0d368293199edff84f6
Aaron <aaron@Aarons-Air.lan>
2022-08-05 12:33:16 -0400

    added effect to scroll to bottom

--- treasure_bay/src/components/Messages/Messages.js
+++ treasure_bay/src/components/Messages/Messages.js
@@ -1,31 +1,47 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import MessagingInput from './MessagingInput'
 import './Messages.css'
 import { useConversations } from '../../context/ConversationsProvider'
 
 function Messages() {
 
-  const { selectedConversationID, messages } = useConversations()
+  const { selectedConversationID, messages, selectedConversation } = useConversations()
 
-  const formatMessages = messages.filter(
-    msg => msg.reciever_id === selectedConversationID
+  const messagesEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [messages]);
+
+  //console.log(messages)
+  const formatMessages = () => messages.filter(
+    msg => msg.receiver_id === selectedConversation.user_id || msg.sender_id === selectedConversation.user_id
   ).map((message, index) => {
+    //console.log(message)
+    //console.log(selectedConversation.user_id)
     return (
-      <div className='message-sent'>
-        <p key={index} className='from-me'>{message.text}</p>
+      <div className={message.sender_id !== selectedConversation.user_id ? 'sent' : 'received'}>
+        <p key={index}>{message.message}</p>
       </div>
     )
   })
 
   return (
-    <div className='messaging-container-test'>
+    <>
       <div className='messages-wrapper'>
-        {formatMessages}
+        {selectedConversation ?
+          formatMessages() :
+          <div>Please Select a Conversation</div>}
+        <div ref={messagesEndRef}></div>
       </div>
       <div className='typing-container'>
         <MessagingInput />
       </div>
-    </div>
+    </>
   )
 }
 
