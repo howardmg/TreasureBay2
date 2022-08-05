3f9c748742bf4479807782e9780bf42a2ba40ae2
Aaron <aaron@Aarons-Air.lan>
2022-08-05 12:35:56 -0400

    added temp user

--- treasure_bay/src/components/MessagingPage/MessagingPage.js
+++ treasure_bay/src/components/MessagingPage/MessagingPage.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Conversations from '../Conversations/Conversations';
 import Messages from '../Messages/Messages'
-// import { io } from 'socket.io-client'
+import axios from 'axios';
+import { useConversations } from '../../context/ConversationsProvider';
+import { UserProvider } from '../../context/UserProvider';
 import './MessagingPage.css'
 
 function MessagingPage() {
+
+    const [user, setUser] = useState({
+        user_id: 2,
+        first_name: "Steve",
+        last_name: "Baker",
+        zipcode: 12345,
+        city: "Austin",
+        state: "Texas",
+        email: "sbaker@test.com",
+        password: "test321",
+        avatar: "https://pixabay.com/get/g5711da399285486c38d711b496067624a855671ca73a12b5326b17a178dfae93ad82595828f659bfb6d1979b2712df811c532274664aee44d244ae28b5048e35_1920.jpg"
+    })
+
+    const { setMessages, setConversations } = useConversations()
+    const ID = localStorage.getItem('id')
+    useEffect(() => {
+        axios.get(`http://localhost:3025/conversations/${ID}`).then((response) => {
+            setConversations(response.data)
+        })
+        axios.get('http://localhost:3025/messages').then((response) => {
+            setMessages(response.data)
+        })
+    }, [setUser])
+
 
     return (
         <div className='messaging-container'>
