7933d589dd3214fef144a7cbf160289bb12e3f2d
Aaron <aaron@Aarons-Air.lan>
2022-08-09 10:42:30 -0400

    added axios all for multiple requests and user from user provider

--- treasure_bay/src/components/MessagingPage/MessagingPage.js
+++ treasure_bay/src/components/MessagingPage/MessagingPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect } from 'react'
 import Conversations from '../Conversations/Conversations';
 import Messages from '../Messages/Messages'
 import axios from 'axios';
@@ -8,28 +8,22 @@ import './MessagingPage.css'
 
 function MessagingPage() {
 
-    const [user, setUser] = useState({
-        user_id: 2,
-        first_name: "Steve",
-        last_name: "Baker",
-        zipcode: 12345,
-        city: "Austin",
-        state: "Texas",
-        email: "sbaker@test.com",
-        password: "test321",
-        avatar: "https://pixabay.com/get/g5711da399285486c38d711b496067624a855671ca73a12b5326b17a178dfae93ad82595828f659bfb6d1979b2712df811c532274664aee44d244ae28b5048e35_1920.jpg"
-    })
+    const { user } = createContext(UserProvider)
 
     const { setMessages, setConversations } = useConversations()
+
+    //will be replaced with user id
     const ID = localStorage.getItem('id')
     useEffect(() => {
-        axios.get(`http://localhost:3025/conversations/${ID}`).then((response) => {
-            setConversations(response.data)
-        })
-        axios.get('http://localhost:3025/messages').then((response) => {
-            setMessages(response.data)
-        })
-    }, [setUser])
+        axios.all([
+            axios.get(`http://localhost:3025/conversations/${ID}`),
+            axios.get('http://localhost:3025/messages')
+        ])
+            .then((response) => {
+                setConversations(response[0].data);
+                setMessages(response[1].data);
+            });
+    }, [user])
 
 
     return (
